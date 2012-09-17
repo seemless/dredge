c0fd379fc32fb606ddc8527faff04febe2c3b265
Matt <matthew.christopher.smith@gmail.com>
2012-09-17 18:59:21 -0400

    watch board for x pages, get threads, check for relevance, get json, ... , profit

--- dredge.py
+++ dredge.py
@@ -1,49 +1,75 @@
 import requests
 from bs4 import BeautifulSoup
+import json
 import sys
+import re
+from collections import Counter
 
 def main(args):
     context = None
     counter = 0
     boards = ['g']
-    #try:
-    #    context = get_context(args[1])
-    #except Exception as e:
-    #    print("Error: unable to get context, exiting.")
-    #    sys.exit(1)
+    num_pages = 5
+    keywords = {'lulsec':5,'lulzsec':5,'hack':1}
+    threshold = 5
+
     for board in boards:
-        nums = gen_thread_numbers(board)
-        threads = gen_thread(board, nums)
-        for thread in threads:
-            print thread
-    #for i in xrange(477000,478000):
-    #    final_url = url_template % ("po",i)
-    #    r = requests.get(final_url)
-    #    if r.ok:
-    #        print(r.content)
-    #        counter += 1
-    #    if i % 10 == 0:
-    #        print(i)
-    #    
-    #print counter
+        
+        thread_nums = gen_thread_numbers(board,num_pages)
+        threads = gen_thread(board, thread_nums)
+        tagged = gen_interesting(threads, keywords,threshold)
+        json_threads = gen_json_threads(tagged, board)
+        for j in json_threads:
+            comments = ''
+            for post in j['posts']:
+                print(post)
+                try:
+                    comments += " "+post['com']
+                except KeyError as e:
+                    continue
+            #replace all the quoting with nothing
+            comment_text = BeautifulSoup(comments).text.replace(">",'')
+            print (comment_text)
+
+def gen_json_threads(tagged_threads, board):
+    url_template = "http://api.4chan.org/%s/res/%d.json"
+    for num in tagged_threads:
+        url = url_template % (board, num)
+        r = requests.get(url)
+        yield json.loads(r.content)
+        
+def gen_interesting(threads, keywords, threshold):
+    count = 0
+    for num,thread in threads:
+        words = re.findall('\w+', BeautifulSoup(thread).text)
+        c = Counter(words)
+        for key in keywords:
+            #multiply by the "relevance" coefficient
+            count += c[key]*keywords[key]
+        if count >= threshold:
+            yield num
+             
+        
 
 def gen_thread(board,numbers):
     #board , thread number
     for num in numbers:
-        url = "https://api.4chan.org/%s/res/%d.json" % (board, num)
+        url = "http://boards.4chan.org/%s/res/%d" % (board, num)
         r = requests.get(url)
-        yield r.content
+        yield (num, r.content)
     
-def gen_thread_numbers(board, page=0):
+def gen_thread_numbers(board, num_pages):
     '''generates thread numbers from the 4chan boards. An optional page
     argument is provided to access non frontpage material.'''
-    url = "http://boards.4chan.org/%s/%d" % (board,page)
-    r = requests.get(url)
-    soup = BeautifulSoup(r.content)
-    threads = soup.find_all(class_="thread")
+    for page in xrange(num_pages):
+    #summarized pages of threads
+        url = "http://boards.4chan.org/%s/%d" % (board,page)
+        r = requests.get(url)
+        soup = BeautifulSoup(r.content)
+        threads = soup.find_all(class_="thread")
     
-    for thread in threads:
-        yield int(str(thread['id'])[1:])
+        for thread in threads:
+            yield int(str(thread['id'])[1:])
         
 def get_context(input_location):
     '''gives the context of the database by providing metadata.
